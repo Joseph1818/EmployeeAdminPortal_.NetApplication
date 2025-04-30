56186d2bfed4553c977549a3fe904da0f7254b3d
Joseph <josephdjamba04@gmail.com>
2025-04-30 17:14:25 +0200

    Add project files.

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    //localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        //Constructor injection
+        private readonly ApplicationDbContext dbContext;
+       
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext=dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult getAllEmployee(){
+            
+            var allEmployees = dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+    }
+}
---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // Property of the collection that will be stored into the database
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
