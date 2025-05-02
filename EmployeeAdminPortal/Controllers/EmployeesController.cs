5d16e1a8e9179dd394c22e7cf7e320bc87408daf
Joseph <josephdjamba04@gmail.com>
2025-05-02 12:55:21 +0200

    Get Single employee & Add one Employee method added

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,4 +1,6 @@
 ﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,18 +13,56 @@ namespace EmployeeAdminPortal.Controllers
     {
         //Constructor injection
         private readonly ApplicationDbContext dbContext;
-       
+
         public EmployeesController(ApplicationDbContext dbContext)
         {
-            this.dbContext=dbContext;
+            this.dbContext = dbContext;
         }
+        //New Action methods
 
+        //1. Get All employees
         [HttpGet]
-        public IActionResult getAllEmployee(){
-            
-            var allEmployees = dbContext.Employees.ToList();
+        public IActionResult getAllEmployee() {
 
+            var allEmployees = dbContext.Employees.ToList();
             return Ok(allEmployees);
         }
+
+        //2.Methods to retrieve a single employee
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult getEmployeeByID (Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee is null )
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(employee);
+            }
+        }
+
+        //3. Add one employee
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            //Coverting AddEmployeeDto to Employee Entity
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                PhoneNumber = addEmployeeDto.PhoneNumber,
+                Salary = addEmployeeDto.Salary
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+        
     }
 }
